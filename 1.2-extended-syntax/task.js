ce233f9118687a03a5ca1ed2535226dae936c53c
Evgeniy-27 <Li.Evgen@yandex.ru>
2020-03-27 00:42:20 +0700

    first commit extended-syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,51 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+"use strict";
+
+function getResult(a, b, c) {
+  // ax2 + bx + c = 0
+  let D = Math.pow(b, 2) - 4 * a * c;
+  const x = [];
+  if (D < 0) {
+    const x = [];
+  } else if (D === 0) {
+    x[0] = ((-b + Math.sqrt(D)) / 2) * a;
+  } else if (D > 0) {
+    x[0] = ((-b + Math.sqrt(D)) / 2) * a;
+    x[1] = ((-b - Math.sqrt(D)) / 2) * a;
+  }
+
+  return x;
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+function getAverageMark(marks) {
+  let averageMark = 0;
+  let sum = 0;
+  if (marks.length === 0) {
+    averageMark = 0;
+  } else if (marks.length > 5) {
+    console.log("Количество оценок больше 5");
+    marks.splice(5);
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+        averageMark = sum / marks.length;
+      }
+  } else {
+    for (let i = 0; i < marks.length; i++) {
+      sum += marks[i];
+      averageMark = sum / marks.length;
+    }
+  }
+  return averageMark;
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+function askDrink(name, dateOfBirthday) {
+    let yearBirth = dateOfBirthday.getFullYear();
+    let yearNow = new Date().getFullYear();
+    let result;
+    let age = yearNow - yearBirth;
+    if (age >= 18){
+        result = `Не желаете ли олд-фэшн, ${name}?`;
+    }else{
+        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
+  return result;
 }
