b5b689d6414816eb8f77c21ef8d3c9ab3ef073a9
Evgeniy-27 <Li.Evgen@yandex.ru>
2020-03-29 10:19:13 +0700

    homeworks_3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,33 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    "use strict";
+
+    if(isNaN(percent)) {
+        return `Параметр процентная ставка содержит неправильное значение`
+    } if(isNaN(contribution)) {
+        return `Параметр начальный взнос содержит неправильное значение`
+    } if(isNaN(amount)) {
+        return `Параметр общая стоимость содержит неправильное значение`
+    } if(isNaN(date)) {
+        return `Введите дату`
+    } 
+    let bodyLoan = amount - contribution;
+    let today = new Date();
+    let mounths = (date.getFullYear() - today.getFullYear()) * 12 + (date.getMonth()) - (today.getMonth());
+    let percentMonth = percent / 12 / 100;
+    let payMounth = bodyLoan * (percentMonth + percentMonth / (Math.pow(1 + percentMonth, mounths) - 1));
+    let totalLoans = payMounth * mounths;
+    let totalAmount = Number(totalLoans.toFixed(2));
+    
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (name === undefined || name === null || name === "") {
+        greeting = `Привет, мир! Меня зовут Аноним`;
+      } else {
+        greeting = `Привет, мир! Меня зовут ${name}`;
+      }
+      //greeting = String(title);
+
+    return greeting;
 }
