cb571b33906691c5d5f647e9d01e950ed372aa0d
Evgeniy-27 <Li.Evgen@yandex.ru>
2020-04-04 23:13:54 +0700

    homework_2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,41 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    let animalSound;
+    if (animal == undefined) {
+        animalSound = null;
+    } else {
+        animalSound = animal.sound;
+    }
+    return animalSound;
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let averageMark = 0;
+    let average = 0;
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+    if (marks.length > 0) {
+        average = sum / marks.length;
+        averageMark = Math.round(average);
+    }
+    return averageMark
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let verdict;
+    let now = Date.now();
+    let Dat = new Date(birthday);
+    let birth = +Dat;
+    let diff = now - birth;
+    let age = diff / 31536000000
+    //console.log(age);
+    if (age > 18) {
+        verdict = true;
+    } else {
+        verdict = false;
+    }
+    //console.log(verdict);
+
+    return verdict
 }
